adb291be3b56b028cbce0b5e0375d03b955e6d3a
vduraphe <duraphe.vaidehi@gmail.com>
2017-03-20 18:39:39 -0500

    added mapview and login

--- SureWalk/SureWalk/RequestViewController.swift
+++ SureWalk/SureWalk/RequestViewController.swift
@@ -7,18 +7,64 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class RequestViewController: UIViewController {
+class RequestViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+
+    enum MapType: NSInteger {
+        case StandardMap = 0
+        case SatelliteMap = 1
+        case HybridMap = 2
+    }
+    
+    var locationManager = CLLocationManager.init()
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
 
+        mapView.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
+        mapView.showsScale = true
+        mapView.showsCompass = true
+        locationManager.requestWhenInUseAuthorization()
+        mapView.mapType = .standard
+        mapView.showsUserLocation = true
+        mapView.showsScale = true
+        mapView.showsCompass = true
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func currentLocation(_ sender: UIBarButtonItem) {
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.0075, longitudeDelta: 0.0075)
+        let region = MKCoordinateRegion.init(center: (locationManager.location?.coordinate)!, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    @IBAction func mapTypeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case MapType.StandardMap.rawValue:
+            mapView.mapType = .standard
+        case MapType.SatelliteMap.rawValue:
+            mapView.mapType = .satellite
+        case MapType.HybridMap.rawValue:
+            mapView.mapType = .hybrid
+        default:
+            break
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView .setCenter(userLocation.coordinate, animated: true)
     }
     
 
